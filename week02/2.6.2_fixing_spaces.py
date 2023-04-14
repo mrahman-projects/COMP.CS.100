d32d505c09b31eacf9b500f70ddcbd0c2436eede
Mijanur Rahman <bilashece@icloud.com>
2023-04-14 13:46:17 +0300

    updated with week02 exercises

---
+++ week02/2.6.2_fixing_spaces.py
@@ -0,0 +1,14 @@
+"""
+This program ...
+
+Project No.Name: <...>
+Name: Mijanur Rahman (mijanur.m.rahman@tuni.fi)
+Student ID: 151762776
+"""
+
+
+def main():
+    # code starts here
+
+if __name__ == "__main__":
+    main()
